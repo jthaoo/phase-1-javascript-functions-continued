25dc0de37109603dea6b0ca66855a752d8763b49
Johnathan Thao <johnathanthao21@gmail.com>
2022-06-14 09:31:26 -0600

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(act='roller-skate') {
+    return 'This Saturday, I want to '+act+'!' ;
+}
+
+const mondayWork = function(activity="go to the office") {
+    return ("This Monday, I will "+activity+".")
+
+}
+
+function wrapAdjective(d) {
+    return function inner(defaul='*'){
+        return 'You are ' + d + defaul + d + '!'
+    }
+}
+
+wrapAdjective("||")("a dedicated programmer")
